7d3ac75e0f9e3a46576150b748f4e7f693186396
purvil3105 <patelpurvil3105@gmail.com>
2025-08-24 17:56:29 +0530

    custom api and error handling

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import connectDB from "./db/index.js";
+import dotenv from "dotenv"
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+})
+.catch((err)=>{
+    console.log("Error connecting to MongoDB:", err);
+})
+
+
+
+
+
+
+
+
+
+//  
+// const app  = express();
+// ;(async () =>{
+//     try {
+//          await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//          app.on("error", (error)=>{
+//             console.log("Express error:", error);
+//             throw error;
+//          })
+//          app.listen(process.env.PORT, () => {
+//             console.log(`Server is running on port ${process.env.PORT}`);
+//          });
+//     } catch (error) {
+//         console.error("Error connecting to MongoDB:", error);
+//         throw error;
+//     }
+// })
